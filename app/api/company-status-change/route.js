51c61fecc5bb1dc9d68739acb152d8607651533b
mahmoud-abo-al-ela <mahmoud.aboalela2786@gmail.com>
2025-07-29 16:45:25 +0300

    fix issue

--- app/api/company-status-change/route.js
+++ app/api/company-status-change/route.js
@@ -3,21 +3,15 @@ import {
   sendApprovalNotification,
   sendRejectionNotification,
 } from "@/lib/email";
-import {
-  isValidSignature,
-  SIGNATURE_HEADER_NAME,
-} from "@sanity/webhook-toolkit";
-
-const SANITY_WEBHOOK_SECRET = process.env.SANITY_WEBHOOK_SECRET; // add this in your .env file
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
+const secret = process.env.SANITY_WEBHOOK_SECRET; // add this in your .env file
 
 export async function POST(req) {
-  const signature = req.headers.get(SIGNATURE_HEADER_NAME);
+  const signature = req.headers[SIGNATURE_HEADER_NAME];
   const bodyText = await req.text(); // read body as text for verification
 
   // Verify signature
-  const isValid = isValidSignature(bodyText, signature, SANITY_WEBHOOK_SECRET);
-  if (!isValid) {
-    console.warn("Invalid webhook signature");
+  if (!(await isValidSignature(bodyText, signature, secret))) {
     return NextResponse.json(
       { success: false, message: "Invalid signature" },
       { status: 401 }
